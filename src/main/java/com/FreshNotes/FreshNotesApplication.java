7eab63cff581f24dfec678543db0de9a5a3c83bf
Akash Mohan CHaudhary <hadt0786@gmail.com>
2019-09-11 23:17:33 +0530

    firstcommit

---
+++ src/main/java/com/FreshNotes/FreshNotesApplication.java
@@ -0,0 +1,13 @@
+package com.FreshNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FreshNotes/FreshNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FreshNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
